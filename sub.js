48135f72e2577f0ab5d5664fbca76cd6103b1462
jackson <sessntlimboo@gmail.com>
2025-01-13 22:38:09 +0530

    code for java

---
+++ sub.js
@@ -0,0 +1,30 @@
+function baby(){
+
+    var  num1,num2;
+
+    num1= parseInt(document.getElementById("rare").value);
+    num2= parseInt(document.getElementById("rare2").value);
+
+    sub=num1-num2;
+
+    document.getElementById("rare3").value=sub;
+
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
