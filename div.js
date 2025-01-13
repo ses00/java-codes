36345762613324bb8fc9110c8e69868ecffd8742
jackson <sessntlimboo@gmail.com>
2025-01-13 23:04:16 +0530

    new files

---
+++ div.js
@@ -0,0 +1,12 @@
+function main(){
+
+    var h1,h2;
+
+    h1= parseInt(document.getElementById("wwe").value);
+    h2= parseInt(document.getElementById("wwe2").value);
+
+    div=h1/h2;
+
+    document.getElementById("wwe3").value=div;
+    
+}
