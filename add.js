b482065d0a58e8ac69afd971489375209ff0122e
jackson <sessntlimboo@gmail.com>
2025-01-14 23:36:47 +0530

    new files

---
+++ add.js
@@ -0,0 +1,12 @@
+function poop(){
+
+    var ses1,ses2;
+
+    ses1= parseInt(document.getElementById("bob").value);
+    ses2= parseInt(document.getElementById("bob2").value);
+
+    add=ses1+ses2;
+
+    document.getElementById("bob3").value=add;
+
+}
